fc824875f3ee3802b0faacf5d65151fa690e1420
omar alshehri <3omr.sa@gmail.com>
2023-05-23 21:02:20 +0300

    add : Profile

--- bin/Route/UserRoute.dart
+++ bin/Route/UserRoute.dart
@@ -3,15 +3,15 @@ import 'package:shelf_router/shelf_router.dart';
 
 import '../Middleware/User/CheckToken_Middleware.dart';
 import '../Response/User/deleteContactResponse.dart';
+import '../Response/User/displayProfileResponse.dart';
 import '../Response/User/userDisplayResponse.dart';
-
-
 
 class UserRoute {
   Handler get handler {
     final router = Router()
-    ..get("/user-display", userDisplayResponse)
-    ..post('/delete-contact', deleteContactResponse);
+      ..get("/user-display", userDisplayResponse)
+      ..post('/delete-contact', deleteContactResponse)
+      ..get('/profile', displayProfileResponse);
     final pipline =
         Pipeline().addMiddleware(checkTokenMiddleware()).addHandler(router);
 
