4b98b3f0e022b1b33f34b3c87e6ce51d45ea6a8c
ALWALEED-KHUSHAIM <doode711.cr7@gmail.com>
2023-05-23 17:33:09 +0300

    ubdate delet Content

--- bin/Route/UserRoute.dart
+++ bin/Route/UserRoute.dart
@@ -2,12 +2,11 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 import '../Middleware/User/CheckToken_Middleware.dart';
-
-
+import '../Response/user/deletcontact/deletcontact.dart';
 
 class UserRoute {
   Handler get handler {
-    final router = Router();
+    final router = Router()..post('/deletcontact', deletcontact);
     final pipline =
         Pipeline().addMiddleware(checkTokenMiddleware()).addHandler(router);
 
