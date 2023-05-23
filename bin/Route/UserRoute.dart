c8c7d500650e9abf2334c2b710dd6b92d735ffb1
ZiyadALharbi <ziyad.swengineer@gmail.com>
2023-05-23 21:37:19 +0300

    hhh

--- bin/Route/UserRoute.dart
+++ bin/Route/UserRoute.dart
@@ -2,6 +2,7 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 import '../Middleware/User/CheckToken_Middleware.dart';
+import '../Response/User/addContactRespone.dart';
 import '../Response/User/deleteContactResponse.dart';
 import '../Response/User/displayProfileResponse.dart';
 import '../Response/User/userDisplayResponse.dart';
@@ -9,14 +10,11 @@ import '../Response/User/userDisplayResponse.dart';
 class UserRoute {
   Handler get handler {
     final router = Router()
-<<<<<<< HEAD
-      ..get("/user-display", userDisplayResponse)
+      ..get("/user-display/<id>", userDisplayResponse)
       ..post('/delete-contact', deleteContactResponse)
-      ..get('/profile', displayProfileResponse)..post('/add_contact', addContactResponse);
-=======
-    ..get("/user-display/<id>", userDisplayResponse)
-    ..post('/delete-contact', deleteContactResponse);
->>>>>>> 5365d2448c90b79ff55b6ecf61de33bd44d11160
+      ..get('/profile', displayProfileResponse)
+      ..post('/add_contact', addContactResponse);
+
     final pipline =
         Pipeline().addMiddleware(checkTokenMiddleware()).addHandler(router);
 
